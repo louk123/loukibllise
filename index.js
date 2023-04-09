cea1697af536697b41439317b8bb53d235e7a302
louk123 <106319242+louk123@users.noreply.github.com>
2023-04-09 05:42:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-say('SHANO BOT', {
+say('louk BOT', {
   font: 'simple3d',
   align: 'center',
   gradient: ['red', 'magenta']
